01b28ba63e3d907d56daccd04dc6a101beb18ae1
Fernando Watanabe <maz_fwatanabe@uolinc.com>
2018-02-12 17:36:45 -0200

    Heroku changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -52,8 +53,6 @@ app.get('/about', (req,res) => {
 app.get('/bad', (req,res) => {
     res.send({ errorMessage : 'Unable to handle request' });
 });
-
-var port = 3000;
 
 app.listen(port, () => {
     console.log(`Server up on port ${port}`);
